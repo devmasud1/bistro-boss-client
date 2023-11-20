b60d156746c3062bee97f5914e615a32d903cbda
devmasud1 <devmahmud.masu1@gmail.com>
2023-11-20 15:06:01 +0600

    admin manage item page

--- src/hooks/UseMenu.jsx
+++ src/hooks/UseMenu.jsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react";
 import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
 
 const UseMenu = () => {
-  const [menu, setMenu] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const axiosSecureUrl = useAxiosSecure();
+  // const [menu, setMenu] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  //const axiosSecureUrl = useAxiosSecure();
 
-  useEffect(() => {
-    axiosSecureUrl.get("/menu").then((res) => {
-      setMenu(res.data);
-      setLoading(false);
-    });
-  }, [axiosSecureUrl]);
-  return [menu, loading];
+  // useEffect(() => {
+  //   axiosSecureUrl.get("/menu").then((res) => {
+  //     setMenu(res.data);
+  //     setLoading(false);
+  //   });
+  // }, [axiosSecureUrl]);
+  // return [menu, loading];
+
+  const axiosPublicUrl = useAxiosPublic();
+
+  const {
+    data: menu = [],
+    isPending: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await axiosPublicUrl.get("/menu");
+      return res.data;
+    },
+  });
+  return [menu, loading, refetch];
 };
 export default UseMenu;
--- src/pages/Dashboard/Admin/ManageItem/ManageItem.jsx
+++ src/pages/Dashboard/Admin/ManageItem/ManageItem.jsx
@@ -1,9 +1,104 @@
+import Swal from "sweetalert2";
 import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import UseMenu from "./../../../../hooks/UseMenu";
+import { Link } from "react-router-dom";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const ManageItem = () => {
-    return(
-        <div>
-              <SectionTitle subHeading="Hurry up!" mainHeading="MANAGE ALL items" />
+  const [menu, ,refetch] = UseMenu();
+  const axiosSecureUrl = useAxiosSecure();
+
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecureUrl.delete(`/menu/${item._id}`);
+        if (res.data.deletedCount > 0) {
+          // refetch to update the ui
+          refetch();
+          toast.success(`${item.name} has been deleted`, {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+
+        }
+      }
+    });
+  };
+
+  return (
+    <div>
+      <SectionTitle subHeading="Hurry up!" mainHeading="MANAGE ALL items" />
+      <h1>ghf{menu.length}</h1>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menu.map((item, index) => (
+                <tr key={item._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={item.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{item.name}</td>
+                  <td className="text-right">${item.price}</td>
+                  <td>
+                    <Link to={`/dashboard/updateItem/${item._id}`}>
+                      <button className="btn btn-ghost btn-lg bg-red-600">
+                        <FaEdit className="text-white"></FaEdit>
+                      </button>
+                    </Link>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteItem(item)}
+                      className="btn btn-ghost btn-lg"
+                    >
+                      <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    )}
+      </div>
+    </div>
+  );
+};
 export default ManageItem;
